29f839a11a9e1b38a9fe299dcccb70f626de7080
Anubhav Saini <iamanubhavsaini@gmail.com>
2014-08-04 15:59:37 +0530

    added just some of docde

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,15 @@
 class FizzBuzz {
-}  
+
+    public String Run(int number) {
+        if(number %15 == 0){
+            return "FizzBuzz";
+        }
+        if(number %5 == 0){
+            return "Buzz";
+        }
+        if(number %3 == 0){
+            return "Fizz";
+        }
+        return String.valueOf(number);
+    }
+}
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,7 +1,24 @@
 
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+
 public class FizzBuzzTest {
 
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    @Test
+    public  void shouldReturnNumberIfNotFizzBuzz() {
+        assertEquals("1", fizzBuzz.Run(1));
+    }
+    @Test
+    public void shouldReturnFizzIfNumberOnlyModThree(){
+        assertEquals("Fizz", fizzBuzz.Run(3));
+        assertEquals("Fizz", fizzBuzz.Run(6));
+        assertEquals("Fizz", fizzBuzz.Run(9));
 
+    }
 }
 
 
